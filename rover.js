625915926fcb018490cc9910aeb8c9bc94e65839
Kyle Darnold <>
2021-04-19 03:19:43 +0000

    test 8

--- rover.js
+++ rover.js
@@ -1,4 +1,18 @@
 class Rover {
+  constructor(position, generatorWatts=110){
+    this.position = position;
+    this.mode = 'NORMAL';
+    this.generatorWatts = generatorWatts;
+    
+  }
+
+  receiveMessage(message){
+    let messageResults = {'name': message.name}
+    if (message.commands){
+      messageResults["results"] = message.commands;
+    }
+    return messageResults;
+  }
    // Write code here!
 }
 
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -7,6 +7,23 @@ const Command = require('../command.js');
 
 
 describe("Rover class", function() {
+  
+
+  //Test 7
+  it("constructor sets position and default values for mode and generatorWatts", function() {
+    let rover = new Rover(64863); 
+    expect(rover.position).toEqual(64863); 
+    expect(rover.generatorWatts).toEqual(110);
+  });
+
+  // Test 8
+  it("response returned by receiveMessage contains name of message", function() {
+    let rover = new Rover(64863);
+    let message = new Message('New Message');
+    let messageResults = rover.receiveMessage(message);
+    let messageName = messageResults['name'];
+    expect(messageName).toEqual('New Message');
+  });
 
   // 7 tests here!
 
